42494dda676389cd0f79eb2d68fc7e8a868b3fec
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-02 22:37:03 +0300

    1

--- src/shared/api/model/constants.ts
+++ src/shared/api/model/constants.ts
@@ -1,5 +1,6 @@
 export const apiDomainAccountDev = 'http://localhost:10005/api'
-export const apiDomainAccountProd = 'https://api.pm.cloudmill.ru'
-export const apiDomain = import.meta.env.NODE_ENV === 'development' ? `${apiDomainAccountDev}` : `${apiDomainAccountProd}`
+// export const apiDomainAccountProd = 'https://api.pm.cloudmill.ru'
+// export const apiDomain = import.meta.env.NODE_ENV === 'development' ? `${apiDomainAccountDev}` : `${apiDomainAccountProd}`
+export const apiDomain = apiDomainAccountDev
 // export const apiDomain = `${apiDomainAccountProd}`
 // export const apiDomainFile = `${apiDomainAccount}`
