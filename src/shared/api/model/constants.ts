18f4f04685fa0682dda9f87741ca505b83fd4b8f
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-05 14:49:08 +0300

    1

--- src/shared/api/model/constants.ts
+++ src/shared/api/model/constants.ts
@@ -1,5 +1,6 @@
 export const apiDomainAccountDev = 'http://localhost:10005/api'
-export const apiDomainAccountProd = 'https://front-test.devmill.ru/api'
+// export const apiDomainAccountProd = 'https://front-test.devmill.ru/api'
+export const apiDomainAccountProd = 'https://big-grain-nest.vercel.app/api'
 // export const apiDomain = import.meta.env.NODE_ENV === 'development' ? `${apiDomainAccountDev}` : `${apiDomainAccountProd}`
-export const apiDomain = `${apiDomainAccountDev}`
+export const apiDomain = `${apiDomainAccountProd}`
 // export const apiDomainFile = `${apiDomainAccount}`
