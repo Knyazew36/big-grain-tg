1514b51721b0241e541c2f9ceb645a3eb9474270
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-06 02:58:01 +0300

    1

--- src/pages/menu-page/MenuPage.tsx
+++ src/pages/menu-page/MenuPage.tsx
@@ -16,11 +16,12 @@ import { Role } from '@/entitites/user/model/user.type'
 
 const MenuPage: FC = () => {
   const user = initDataUser()
-  const { data: role, isLoading } = useUserRole(user?.id?.toString() ?? '')
+  // const = use
+  // // const { data: role, isLoading } = useUserRole(user?.id?.toString() ?? '')
 
-  if (isLoading) {
-    return <Loader />
-  }
+  // if (isLoading) {
+  //   return <Loader />
+  // }
 
   return (
     <Page back={false}>
