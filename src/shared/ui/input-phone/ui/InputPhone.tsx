f27f20253fb6500104076be5cd955b3be9e583ad
knyazew.sv@gmail.com <knyazew.sv@gmail.com>
2025-07-09 23:54:23 +0300

    1

--- src/shared/ui/input-phone/ui/InputPhone.tsx
+++ src/shared/ui/input-phone/ui/InputPhone.tsx
@@ -20,6 +20,7 @@ const InputPhone: FC<IProps> = forwardRef<HTMLInputElement, IProps>(
           className='py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
           mask='+7 (___) ___-__-__'
           replacement={{ _: /\d/ }}
+          inputMode='tel'
           onPaste={e => {
             const pasted = e.clipboardData.getData('Text')
             let cleaned = pasted.replace(/\D/g, '')
